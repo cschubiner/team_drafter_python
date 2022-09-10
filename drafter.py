5fd799ffd5c64e343216a7e9cd3542231f09857c
clayschubiner <cschubiner@gmail.com>
2022-09-10 16:26:49 -0700

    more

--- drafter.py
+++ drafter.py
@@ -25,6 +25,45 @@ def modifyJson(yml):
             team["team score"] = sum([player_to_score[player['name']] for player in team["players"]])
             team['team num_players'] = len(team['players'])
             team['team number'] = i + 1
+
+    # Also, add in "proposed_team_1" and "proposed_team_2" to each match
+    # Create the proposed teams by enumerating all possible combinations of players
+    # in which the team size is equal or +1/-1 of each other
+    # Then, for each match, add in the proposed teams
+
+    possible_teams = []
+    total_players = len(player_to_score)
+    players_and_scores = sorted(player_to_score.items(), key=lambda x: x[1], reverse=True)
+    # now shuffle it
+    import random
+
+    best_min_score_delta = 999
+
+    # Choose 100 random teams
+    for i in range(100):
+        random.shuffle(players_and_scores)
+        team1 = []
+        team2 = []
+        # First put half of the players on team 1
+        for j in range(total_players // 2):
+            team1.append(players_and_scores[j])
+        # Then put the remaining on team 2
+        for j in range(total_players // 2, total_players):
+            team2.append(players_and_scores[j])
+        # Now, calculate the score delta
+        team1_score = sum([x[1] for x in team1])
+        team2_score = sum([x[1] for x in team2])
+        score_delta = abs(team1_score - team2_score)
+        if score_delta < best_min_score_delta:
+            best_min_score_delta = score_delta
+            best_team1 = team1
+            best_team2 = team2
+            print("New best team found with score delta of {}".format(score_delta))
+            # Print with teams sorted by score
+            print("Team 1 (score: {} #players: {}): {}".format(team1_score, len(team1), sorted(team1, key=lambda x: x[1], reverse=True)))
+            print("Team 2 (score: {} #players: {}): {}".format(team2_score, len(team2), sorted(team2, key=lambda x: x[1], reverse=True)))
+
+
     return yml
 
 
