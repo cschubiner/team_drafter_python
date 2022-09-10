3a80d8b6629d494467d85fa16b9825451de25ca4
clayschubiner <cschubiner@gmail.com>
2022-09-10 16:12:57 -0700

    more

--- drafter.py
+++ drafter.py
@@ -11,40 +11,44 @@ from watchdog.events import LoggingEventHandler
 
 CURRENT_TEAM_FILENAME: str = "current_teams.yml"
 
+
 def modifyJson(yml):
-  for i, match in enumerate(yml["matches"]):
-    match["_match number"] = i + 1
-    for team in match["teams"]:
-      team["team score"] = sum([int(player['name'].split(' ')[-1]) for player in team["players"]])
-      team['team num_players'] = len(team['players'])
-  return yml
+    for i, match in enumerate(yml["matches"]):
+        match["_match number"] = i + 1
+        for team in match["teams"]:
+            team["team score"] = sum([int(player['name'].split(' ')[-1]) for player in team["players"]])
+            team['team num_players'] = len(team['players'])
+    return yml
+
 
 def modifyFile(path):
-   yaml_dump = None
-   with open(path, 'r') as stream:
-       try:
+    yaml_dump = None
+    with open(path, 'r') as stream:
+        try:
             parsed = yaml.safe_load(stream)
             newYaml = modifyJson(parsed)
             yaml_dump = yaml.dump(newYaml)
             # print(yaml_dump)
-       except yaml.YAMLError as exc:
-           print(exc)
-           print("NOT YAML")
-       with open(path, 'w') as stream:
-           try:
-               stream.write(yaml_dump)
-           except yaml.YAMLError as exc:
-               print("could not write")
+        except yaml.YAMLError as exc:
+            print(exc)
+            print("NOT YAML")
+        with open(path, 'w') as stream:
+            try:
+                stream.write(yaml_dump)
+            except yaml.YAMLError as exc:
+                print("could not write")
+
 
 class MyHandler(PatternMatchingEventHandler):
-   patterns = ["*" + CURRENT_TEAM_FILENAME]
+    patterns = ["*" + CURRENT_TEAM_FILENAME]
 
-   def on_modified(self, event):
-       # print(event.src_path, event.event_type)
-       modifyFile(event.src_path)
+    def on_modified(self, event):
+        # print(event.src_path, event.event_type)
+        modifyFile(event.src_path)
 
-if __name__ == "__main__":
-  modifyFile("text_files/" + CURRENT_TEAM_FILENAME)
+
+if __name__ == "__main__" or __name__ == "builtins":
+    modifyFile("text_files/" + CURRENT_TEAM_FILENAME)
 
 # if __name__ == "__main__":
 if __name__ == "__main__" and False:
