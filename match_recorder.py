37f8c850c261af3132e36b7c32e2ed96b71f4324
clayschubiner <cschubiner@gmail.com>
2023-11-11 16:55:18 -0800

    aider: Modify `append_to_csv` function in `match_recorder.py` to record ties appropriately and distinguish the teams in the event of a tie.
    # Aider chat conversation:
    
    ASSISTANT: To address the issue of recording ties appropriately and distinguishing the teams in the event of a tie, we need to modify the `append_to_csv` function in the `match_recorder.py` file. We will add a condition to check for a tie and then record the result as "Tie as Team 1" or "Tie as Team 2" accordingly.
    
    Here's the change that needs to be made:
    
    ```python
    match_recorder.py
    <<<<<<< SEARCH
                        player_row.append('Win' if score_team1 > score_team2 else 'Lose')
                    elif any(normalized_player in p for p in team2_players):
                        player_row.append('Win' if score_team2 > score_team1 else 'Lose')
    =======
                        if score_team1 == score_team2:
                            player_row.append('Tie as Team 1')
                        else:
                            player_row.append('Win' if score_team1 > score_team2 else 'Lose')
                    elif any(normalized_player in p for p in team2_players):
                        if score_team1 == score_team2:
                            player_row.append('Tie as Team 2')
                        else:
                            player_row.append('Win' if score_team2 > score_team1 else 'Lose')
    >>>>>>> REPLACE
    ```
    
    This change adds a check for a tie by comparing `score_team1` and `score_team2`. If they are equal, it appends "Tie as Team 1" or "Tie as Team 2" to the `player_row` list depending on which team the player is in. If it's not a tie, it appends "Win" or "Lose" as before. This way, the CSV will accurately reflect ties and indicate the team each player was on during the tie.

--- match_recorder.py
+++ match_recorder.py
@@ -76,9 +76,15 @@ def append_to_csv(filename, unique_players, rounds_data, match_notes):
                 # Check if the normalized player name is in the teams
                 # Check if the normalized player name is a substring of any player name in the teams
                 if any(normalized_player in p for p in team1_players):
-                    player_row.append('Win' if score_team1 > score_team2 else 'Lose')
+                    if score_team1 == score_team2:
+                        player_row.append('Tie as Team 1')
+                    else:
+                        player_row.append('Win' if score_team1 > score_team2 else 'Lose')
                 elif any(normalized_player in p for p in team2_players):
-                    player_row.append('Win' if score_team2 > score_team1 else 'Lose')
+                    if score_team1 == score_team2:
+                        player_row.append('Tie as Team 2')
+                    else:
+                        player_row.append('Win' if score_team2 > score_team1 else 'Lose')
                 else:
                     player_row.append('N/A')  # Player did not participate in this round
             writer.writerow(player_row)
