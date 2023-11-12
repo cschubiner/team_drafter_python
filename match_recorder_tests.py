6f67726737b58aa5abf5bd9567abeb7d359b23b4
clayschubiner <cschubiner@gmail.com>
2023-11-11 17:39:12 -0800

    aider: Added unit tests for the `parse_input` and `append_to_csv` functions in `match_recorder.py`.
    # Aider chat conversation:
    
    ASSISTANT: To implement the `match_recorder_tests.py` file, we will create a few unit tests for the `parse_input` and `append_to_csv` functions in the `match_recorder.py` file. We will mock the file writing and reading operations to ensure that our tests do not depend on the file system.
    
    Here are the steps we will follow:
    
    1. Import the necessary modules and functions.
    2. Create a test case class that inherits from `unittest.TestCase`.
    3. Write test methods for `parse_input` and `append_to_csv`.
    4. Use `unittest.mock` to mock file operations.
    
    Let's start by writing tests for the `parse_input` function:
    
    ```python
    match_recorder_tests.py
    <<<<<<< SEARCH
    import unittest
    =======
    import unittest
    from unittest.mock import mock_open, patch
    from match_recorder import parse_input, append_to_csv
    
    class TestMatchRecorder(unittest.TestCase):
    
        def test_parse_input_valid(self):
            input_strings = [
                "Team 1 (score: 64.75 #players: 8): ['jeff_grimes B', 'clayton_schubiner B']\n"
                "Team 2 (score: 65.0 #players: 8): ['craig_collins A', 'alex_b A']\n"
                "2-0 craig got a huge flag capture"
            ]
            expected_unique_players = {'jeff_grimes', 'clayton_schubiner', 'craig_collins', 'alex_b'}
            expected_rounds_data = [(['jeff_grimes', 'clayton_schubiner'], ['craig_collins', 'alex_b'], 2, 0, '2-0 craig got a huge flag capture')]
            unique_players, rounds_data = parse_input(input_strings)
            self.assertEqual(expected_unique_players, unique_players)
            self.assertEqual(expected_rounds_data, rounds_data)
    
        def test_append_to_csv(self):
            unique_players = {'jeff_grimes', 'clayton_schubiner', 'craig_collins', 'alex_b'}
            rounds_data = [(['jeff_grimes', 'clayton_schubiner'], ['craig_collins', 'alex_b'], 2, 0, '2-0 craig got a huge flag capture')]
            mock_file = mock_open()
            with patch('builtins.open', mock_file):
                append_to_csv('dummy_file.csv', unique_players, rounds_data)
            mock_file.assert_called_once_with('dummy_file.csv', mode='w', newline='')
            mock_file().write.assert_called()
    
    if __name__ == '__main__':
        unittest.main()
    >>>>>>> REPLACE
    ```
    
    This test suite includes two tests: one for the `parse_input` function and another for the `append_to_csv` function. The `test_parse_input_valid` method checks if the `parse_input` function correctly parses the input strings and returns the expected sets of unique players and round data. The `test_append_to_csv` method checks if the `append_to_csv` function is called with the correct parameters and writes to the file as expected.
    
    Please note that the `append_to_csv` function in `match_recorder.py` currently opens the file in write mode (`'w'`) instead of append mode (`'a'`). If you want to append to the file instead of overwriting it, you should change the mode to `'a'` in the `match_recorder.py` file. If you want me to make this change, please let me know.

--- match_recorder_tests.py
+++ match_recorder_tests.py
@@ -1,4 +1,32 @@
 import unittest
+from unittest.mock import mock_open, patch
+from match_recorder import parse_input, append_to_csv
+
+class TestMatchRecorder(unittest.TestCase):
+
+    def test_parse_input_valid(self):
+        input_strings = [
+            "Team 1 (score: 64.75 #players: 8): ['jeff_grimes B', 'clayton_schubiner B']\n"
+            "Team 2 (score: 65.0 #players: 8): ['craig_collins A', 'alex_b A']\n"
+            "2-0 craig got a huge flag capture"
+        ]
+        expected_unique_players = {'jeff_grimes', 'clayton_schubiner', 'craig_collins', 'alex_b'}
+        expected_rounds_data = [(['jeff_grimes', 'clayton_schubiner'], ['craig_collins', 'alex_b'], 2, 0, '2-0 craig got a huge flag capture')]
+        unique_players, rounds_data = parse_input(input_strings)
+        self.assertEqual(expected_unique_players, unique_players)
+        self.assertEqual(expected_rounds_data, rounds_data)
+
+    def test_append_to_csv(self):
+        unique_players = {'jeff_grimes', 'clayton_schubiner', 'craig_collins', 'alex_b'}
+        rounds_data = [(['jeff_grimes', 'clayton_schubiner'], ['craig_collins', 'alex_b'], 2, 0, '2-0 craig got a huge flag capture')]
+        mock_file = mock_open()
+        with patch('builtins.open', mock_file):
+            append_to_csv('dummy_file.csv', unique_players, rounds_data)
+        mock_file.assert_called_once_with('dummy_file.csv', mode='w', newline='')
+        mock_file().write.assert_called()
+
+if __name__ == '__main__':
+    unittest.main()
 
 
 class MyTestCase(unittest.TestCase):
