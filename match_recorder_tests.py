e0dc2bbd4cd76758bb5106cb1d2a9d1bd26143f5
clayschubiner <cschubiner@gmail.com>
2023-11-11 17:46:53 -0800

    Refactor expected_csv_output in match_recorder_tests.py to include player names and match results.

--- match_recorder_tests.py
+++ match_recorder_tests.py
@@ -25,7 +25,7 @@ Team 2 (score: 66.75 #players: 8): ['craig_collins A', 'alex_b A', 'arthur_orcha
 """
     ]
         expected_csv_output = "Player Name,Round 1,Round 2,Round 3,Round 4" + os.linesep + \
-Alex_Mark,Win,Tie as Team 1,Win,Tie as Team 1
+"""Alex_Mark,Win,Tie as Team 1,Win,Tie as Team 1
 Zach_Costa,Lose,Tie as Team 2,Lose,Tie as Team 2
 alex_b,Lose,Tie as Team 2,Lose,Tie as Team 2
 andrew_carmine,Win,Tie as Team 1,Lose,Tie as Team 1
